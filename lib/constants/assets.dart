703eca0c2654d7b89727fa4b667376f652198fe5
Zakarie <116062502+zakariem@users.noreply.github.com>
2024-08-01 09:30:07 +0300

    Add Assets and Colors Constants, Refactor Main, and Update Dependencies
    - Created `assets.dart` in `lib/constants/` to centralize asset paths for images and icons.
    - Introduced `colors.dart` in `lib/constants/` to define color constants used throughout the app.
    - Removed redundant `icons.dart` as its content is now consolidated in `assets.dart`.
    - Refactored `main.dart` to remove commented-out and unnecessary code, improving readability and maintainability.
    - Updated `pubspec.yaml` to include new dependencies (`flutter_svg`, `google_fonts`) and correct asset paths.
    - Updated `pubspec.lock` to reflect the new dependencies and their versions.
    - Updated `GeneratedPluginRegistrant.swift` to register `PathProviderPlugin` for macOS.

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,6 +1,6 @@
 class Assets {
   Assets._();
-  
+
   /// Assets for imagesBackground
   /// assets/images/background.jpg
   static const String imagesBackground = "assets/images/background.jpg";
@@ -44,5 +44,28 @@ class Assets {
   /// Assets for imagesProject06
   /// assets/images/project-06.jpg
   static const String imagesProject06 = "assets/images/project-06.jpg";
-}
 
+  /// Assets for iconsCheck
+  /// assets/icons/check.svg
+  static const String iconsCheck = "assets/icons/check.svg";
+
+  /// Assets for iconsDownload
+  /// assets/icons/download.svg
+  static const String iconsDownload = "assets/icons/download.svg";
+
+  /// Assets for iconsDribble
+  /// assets/icons/dribble.svg
+  static const String iconsDribble = "assets/icons/dribble.svg";
+
+  /// Assets for iconsGithub
+  /// assets/icons/github.svg
+  static const String iconsGithub = "assets/icons/github.svg";
+
+  /// Assets for iconsLinkedin
+  /// assets/icons/linkedin.svg
+  static const String iconsLinkedin = "assets/icons/linkedin.svg";
+
+  /// Assets for iconsTwitter
+  /// assets/icons/twitter.svg
+  static const String iconsTwitter = "assets/icons/twitter.svg";
+}
